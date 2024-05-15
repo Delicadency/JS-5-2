a4634b4dd6e25ac45f215d637bc9c3d023b49a8f
Agata Kamińska <a.fluffypie@gmail.com>
2024-05-15 21:39:57 +0200

    added conditional operator

---
+++ script.js
@@ -0,0 +1,13 @@
+//Stwórz funkcję filterItems, która będzie posiadała dwa argumenty typu Array. Funkcja ta z otrzymanych w parametrach dwóch tablic liczb całkowitych, przefiltruje je w taki sposób aby wydobyć tylko liczby większe od zera, a następnie zwróci je obie w postaci jednej połączonej tablicy.
+
+function filterItems (array, array2) {
+    if (![array, array2].every(Array.isArray)) {
+        return "Wprowadzone dane nie są tablicami. Wprowadź wartości dla obu tablic.";
+    } else {
+        const numbersLesserThan0 // dokończ const dodając filter
+        //drugi const liczby większe od 0
+        //albo zostawić tylko drugi
+        //napisz warunek jeśli dane tablic nie są liczbami
+        //zwróć wynik funkcji w postaci dwóch nowych tablic
+    }
+}
