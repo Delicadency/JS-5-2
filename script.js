6d977eda491f9542254460bdcfa370012833a436
Agata Kamińska <a.fluffypie@gmail.com>
2024-05-16 18:09:03 +0200

    whole funcion, deleted comment

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-//Stwórz funkcję filterItems, która będzie posiadała dwa argumenty typu Array. Funkcja ta z otrzymanych w parametrach dwóch tablic liczb całkowitych, przefiltruje je w taki sposób aby wydobyć tylko liczby większe od zera, a następnie zwróci je obie w postaci jednej połączonej tablicy.
-
-function filterItems (array, array2) {
-    if (![array, array2].every(Array.isArray)) {
-        return "Wprowadzone dane nie są tablicami. Wprowadź wartości dla obu tablic.";
-    } else {
-        const numbersLesserThan0 // dokończ const dodając filter
-        //drugi const liczby większe od 0
-        //albo zostawić tylko drugi
-        //napisz warunek jeśli dane tablic nie są liczbami
-        //zwróć wynik funkcji w postaci dwóch nowych tablic
-    }
+function filterItems(array, array2) {
+  if (![array, array2].every(Array.isArray)) {
+    return "Wprowadzone dane nie są tablicami. Wprowadź dwie tablice składające się z liczb.";
+  } else {
+    const numbersBiggerThan0 = [...array, ...array2].filter((n) => n > 0);
+    return { numbersBiggerThan0 };
+  }
 }
